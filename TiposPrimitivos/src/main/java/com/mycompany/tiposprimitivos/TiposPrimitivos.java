3b5971f173d4d181390b8951523e9b075f505e62
Matheus Fernandes <matheusfernandesm2411@gmail.com>
2026-01-30 11:11:11 -0300

    up

---
+++ TiposPrimitivos/src/main/java/com/mycompany/tiposprimitivos/TiposPrimitivos.java
@@ -0,0 +1,15 @@
+package com.mycompany.tiposprimitivos;
+
+public class TiposPrimitivos {
+
+    public static void main(String[] args) {
+
+        String nome = "Gustavo";
+        float nota = 8.5f;
+        System.out.print ("A nota de é: \n" + nota);
+        System.out.println ("A nota de é: " + nota);
+        System.out.printf ("A nota de %s é: %.2f \n", nome, nota);
+        System.out.format ("A nota de %s é: %.2f \n", nome, nota);
+        
+    }
+}
