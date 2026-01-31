b7ce961d59e03f204c29df4a5d99515a92cf7472
Matheus Fernandes <matheusfernandesm2411@gmail.com>
2026-01-31 18:42:14 -0300

    up

--- TiposPrimitivos/src/main/java/com/mycompany/tiposprimitivos/TiposPrimitivos.java
+++ TiposPrimitivos/src/main/java/com/mycompany/tiposprimitivos/TiposPrimitivos.java
@@ -1,14 +1,16 @@
 package com.mycompany.tiposprimitivos;
 
+import java.util.Scanner;
+
 public class TiposPrimitivos {
 
     public static void main(String[] args) {
 
-        String nome = "Gustavo";
-        float nota = 8.5f;
-        System.out.print ("A nota de é: \n" + nota);
-        System.out.println ("A nota de é: " + nota);
-        System.out.printf ("A nota de %s é: %.2f \n", nome, nota);
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Digite o nome do aluno");
+        String nome = teclado.nextLine();
+        System.out.println("Digite a nota do aluno");
+        float nota = teclado.nextFloat();
         System.out.format ("A nota de %s é: %.2f \n", nome, nota);
         
     }
