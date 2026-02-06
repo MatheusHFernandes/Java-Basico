f1df3c6ed1dd9d0e297efca4bb48d0fa5bc41bc5
Matheus Fernandes <matheusfernandesm2411@gmail.com>
2026-02-06 09:28:16 -0300

    FOR

---
+++ RepeticaoFor/src/main/java/com/mycompany/repeticaofor/RepeticaoFor.java
@@ -0,0 +1,11 @@
+package com.mycompany.repeticaofor;
+
+public class RepeticaoFor {
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i <= 100; i+=10) { //DECLARAÇÃO, CONDIÇÃO, CONTAGEM
+            System.out.println("cambalhota " + i);
+        }
+    }
+}
