d02f86f3f62fc15c38fc54e99b4608ef031b8428
Matheus Fernandes <matheusfernandesm2411@gmail.com>
2026-02-06 09:00:01 -0300

    Interface gráfica JOptionPane

---
+++ RepitaOptionPane/src/main/java/com/mycompany/repitaoptionpane/RepitaOptionPane.java
@@ -0,0 +1,20 @@
+package com.mycompany.repitaoptionpane;
+
+import javax.swing.JOptionPane;
+
+public class RepitaOptionPane {
+
+    public static void main(String[] args) {
+        
+        int n,s = 0;
+        
+        do {
+            
+          n = Integer.parseInt (JOptionPane.showInputDialog("<html>Informe um número: <br><em>(valor 0 interrompe)</em></html> "));
+        
+          s += n;
+        } while (n != 0);
+        JOptionPane.showMessageDialog(null, "<>htmlResultado final <hr><br>Somatorio vale " + s + "</html>");
+        
+    }
+}
