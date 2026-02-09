aaafd6145b0dedfc6667b4d99dccf19b8c439c32
Matheus Fernandes <matheusfernandesm2411@gmail.com>
2026-02-09 08:26:28 -0300

    fucvao

---
+++ Funcao/src/main/java/com/mycompany/funcao/Funcao.java
@@ -0,0 +1,15 @@
+package com.mycompany.funcao;
+
+public class Funcao {
+
+    static int soma (int a, int b){
+        int s = a + b;
+        return s;
+    }
+    
+    public static void main(String[] args) {
+        
+        int sm = soma(5,2);
+        System.out.println("A soma vale " + sm);
+    }
+}
